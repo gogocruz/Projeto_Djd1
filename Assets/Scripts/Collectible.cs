8fe1d29e1bd7b33f09a43134f6be21d7d7cd67b6
Jose Reis <josereis.dev@gmail.com>
2021-05-17 21:29:52 +0100

    Update Collectible script

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -7,18 +7,6 @@ public class Collectible : MonoBehaviour
     [SerializeField]
     private int scoreIncrease;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void OnTriggerEnter2D(Collider2D collision)
     {
         Player player = collision.GetComponent<Player>();
