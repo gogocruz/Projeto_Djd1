b13bbf237214349d1413b58b37af9ec8dc13343c
Diogo <eldiogo2002@gmail.com>
2021-05-18 18:15:46 +0100

    Add new sprites, fix Colisionboxes

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -9,7 +9,9 @@ public class Timer : MonoBehaviour
     TextMeshProUGUI countDownText;
 
     private float currentTime = 0f;
-    private float startingTime = 10f;
+    
+    [SerializeField]
+    private float startingTime = 20f;
     private bool startCount;
 
     // Start is called before the first frame update
@@ -28,12 +30,18 @@ public class Timer : MonoBehaviour
             currentTime -= 1 * Time.deltaTime;
             //countDownText.text = $"{currentTime.ToString():f4}";
             countDownText.text = System.String.Format("{0:0.00}", currentTime);
+            Invoke("ResetScene", startingTime);
 
             if (currentTime <= 0)
             {
-                countDownText.text = "0:00";
+                countDownText.text = "0.00";
+                
             }
         }
+    }
+    public void ResetScene()
+    {
+        Application.LoadLevel(Application.loadedLevel);
     }
 
     public void Activate()
