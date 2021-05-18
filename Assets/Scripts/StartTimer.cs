9e02c3ac0df5d779ef8c67b57cfca479fd25cf3b
Jose Reis <josereis.dev@gmail.com>
2021-05-18 01:15:27 +0100

    Add countdown timer

---
+++ Assets/Scripts/StartTimer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartTimer : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player player = collision.GetComponent<Player>();
+
+        if (player != null)
+        {
+            Timer timer = FindObjectOfType<Timer>(true);
+            Debug.Log("Player is here");
+            timer.Activate();
+        }
+        else
+        {
+            Debug.Log("Did't find Player");
+        }
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI countDownText;
+
+    private float currentTime = 0f;
+    private float startingTime = 10f;
+    private bool startCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countDownText.enabled = false;
+        currentTime = startingTime;
+        startCount = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startCount)
+        {
+            currentTime -= 1 * Time.deltaTime;
+            //countDownText.text = $"{currentTime.ToString():f4}";
+            countDownText.text = System.String.Format("{0:0.00}", currentTime);
+
+            if (currentTime <= 0)
+            {
+                countDownText.text = "0:00";
+            }
+        }
+    }
+
+    public void Activate()
+    {
+        countDownText.enabled = true;
+        startCount = true;
+    }
+}
