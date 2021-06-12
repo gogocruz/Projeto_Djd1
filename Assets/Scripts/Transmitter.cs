1128d198f8d210acf6ebe7e5180ae7c60e7c7574
Jose Reis <josereis.dev@gmail.com>
2021-06-12 02:28:19 +0100

    Add Waterlevel win condition

---
+++ Assets/Scripts/Transmitter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transmitter : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Player player = collision.GetComponent<Player>();
+        if (player != null)
+        {
+            player.CanFinishLevel = true;
+            Destroy(gameObject);
+        }
+    }
+}
