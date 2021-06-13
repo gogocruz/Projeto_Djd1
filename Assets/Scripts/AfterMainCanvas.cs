c2d81c68603ebd2ab4bf1b6e6cbb8bcc49ff5788
Jose Reis <josereis.dev@gmail.com>
2021-06-13 18:05:10 +0100

    Add WaterPlanet briefing scene

---
+++ Assets/Scripts/AfterMainCanvas.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AfterMainCanvas : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene("WaterPlanet");
+    }
+}
